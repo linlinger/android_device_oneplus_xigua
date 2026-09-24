91576f87a80023d16ab3ee2e1adce948230600f0
林凌儿 <32541118+linlinger@users.noreply.github.com>
2026-09-24 21:32:08 +0800

    fix: 编译错误修复 (IrisBypass Provider + sepolicy)

--- parts/src/com/aviumui/irisbypass/ConfigPanelSearchIndexablesProvider.java
+++ parts/src/com/aviumui/irisbypass/ConfigPanelSearchIndexablesProvider.java
@@ -16,23 +16,32 @@
 
 package com.aviumui.irisbypass;
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.MatrixCursor;
-import android.net.Uri;
-import android.provider.SearchIndexablesContract;
+import android.provider.SearchIndexableResource;
 import android.provider.SearchIndexablesProvider;
 
-public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
-    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
 
-    private static SearchIndexablesContract.XmlResourceIndexable[] INDEXABLE_RES =
-            new SearchIndexablesContract.XmlResourceIndexable[] {
-                new SearchIndexablesContract.XmlResourceIndexable(1, R.xml.iris_bypass_settings,
-                        IrisBypassSettingsActivity.class.getName(),
-                        R.drawable.ic_settings_device),
-            };
+/**
+ * 让 IRIS bypass 设置在系统设置里可被搜索。
+ */
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.iris_bypass_settings,
+                    IrisBypassSettingsActivity.class.getName(),
+                    R.drawable.ic_settings_device),
+    };
 
     @Override
     public boolean onCreate() {
@@ -41,8 +50,8 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
 
     @Override
     public Cursor queryXmlResources(String[] projection) {
-        MatrixCursor cursor = new MatrixCursor(SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS);
-        for (SearchIndexablesContract.XmlResourceIndexable sir : INDEXABLE_RES) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        for (SearchIndexableResource sir : INDEXABLE_RES) {
             cursor.addRow(generateResourceRef(sir));
         }
         return cursor;
@@ -50,28 +59,25 @@ public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvide
 
     @Override
     public Cursor queryRawData(String[] projection) {
-        MatrixCursor cursor = new MatrixCursor(SearchIndexablesContract.INDEXABLES_RAW_COLUMNS);
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
         return cursor;
     }
 
     @Override
     public Cursor queryNonIndexableKeys(String[] projection) {
-        MatrixCursor cursor = new MatrixCursor(
-                SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS);
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
         return cursor;
     }
 
-    private static Object[] generateResourceRef(SearchIndexablesContract.XmlResourceIndexable sir) {
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
         Object[] ref = new Object[7];
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK] = sir.rank;
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION] =
-                "com.android.settings.action.EXTRA_SETTINGS";
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] =
-                "com.aviumui.irisbypass";
-        ref[SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "com.aviumui.irisbypass";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
         return ref;
     }
 }
